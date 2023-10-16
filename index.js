9ebf352400bfb5e7e248c13c395dd7de2cbe5e27
Zahid Hasan <zahidhasan140338@gmail.com>
2023-10-16 11:38:41 -0700

    created a process to receive data from frontend

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require ('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
 
 const users = [
     {id: 1, name: 'Zahid Hasan', email: 'Zahid1306@gmail.com'},
@@ -15,6 +19,14 @@ app.get('/', (req, res) => {
 
 app.get('/users', (req, res) => {
     res.send(users);
+})
+
+app.post('/users', (req, res) => {
+    console.log(req.body);
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser);
+    res.send(newUser);
 })
 
 
